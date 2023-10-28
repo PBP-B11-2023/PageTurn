162457e0fa984f193c9d7c7a2e1012c9048123ac
fernkhr <fern.khairunnisha@ui.ac.id>
2023-10-29 00:42:23 +0700

    update css

--- request_buku/urls.py
+++ request_buku/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from request_buku.views import show_request, create_request, show_request_json, get_request_json, add_request_ajax
+from request_buku.views import show_request, create_request, show_request_json, get_request_json, add_request_ajax, delete_request
 from django.contrib import admin
 
 app_name = 'request_buku'
@@ -10,4 +10,5 @@ urlpatterns = [
     path('show_request_json/', show_request_json, name='show_request_json'),
     path('get-request/', get_request_json, name='get_request_json'),
     path('create-request-ajax/', add_request_ajax, name='add_request_ajax'),
+    path('delete/<int:id>', delete_request, name='delete_request'),
 ]
