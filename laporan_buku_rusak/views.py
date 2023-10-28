e4eb0f85e52482f3d749a4d29d3b66c1bc25906e
rifdahnabilah23 <rifdah.nabilah@ui.ac.id>
2023-10-28 10:17:52 +0700

    penambahan ajax

--- laporan_buku_rusak/views.py
+++ laporan_buku_rusak/views.py
@@ -4,6 +4,9 @@ from laporan_buku_rusak.forms import ProductForm
 from django.urls import reverse
 from laporan_buku_rusak.models import Product
 from peminjaman.models import Peminjaman
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseNotFound
+from django.core import serializers
 
 # Create your views here.
 def show_laporan(request):
@@ -25,3 +28,21 @@ def create_laporan(request):
     context = {'form': form}
     return render(request, "create_laporan.html", context)
 
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, description=description)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
