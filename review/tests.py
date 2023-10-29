5fe81f394165d5dc8f67bf96c619ebe04c22e166
kkuratolodon <mif05123@gmail.com>
2023-10-29 14:11:47 +0700

    nambahin testing peminjaman

--- review/tests.py
+++ review/tests.py
@@ -1,10 +1,11 @@
-from django.test import TestCase, Client
+from django.test import Client, TestCase
+
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
-        response = Client().get('/main/')
+        response = Client().get('/review/')
         self.assertEqual(response.status_code, 200)
 
     def test_main_using_main_template(self):
-        response = Client().get('/main/')
+        response = Client().get('/review/')
         self.assertTemplateUsed(response, 'reviewbuku.html')
