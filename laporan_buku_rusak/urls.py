8b074d992c40636f584972633e741d04d21a76c1
rifdahnabilah23 <rifdah.nabilah@ui.ac.id>
2023-10-29 14:38:11 +0700

    progress

--- laporan_buku_rusak/urls.py
+++ laporan_buku_rusak/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from laporan_buku_rusak.views import show_laporan, create_laporan, get_product_json, add_product_ajax
+from laporan_buku_rusak.views import show_laporan, get_product_json, add_product_ajax,filter_products
 
 app_name = 'laporan_buku_rusak'
 
 urlpatterns = [
     path('', show_laporan, name='show_laporan'),
-    path('create-laporan', create_laporan, name='create_laporan'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('filter_products/', filter_products, name='filter_products'),
 ]
